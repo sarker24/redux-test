c09047a354e6682d18049d88633d545334cc0db2
murad <mosa@esmiley.dk>
2021-06-02 15:39:31 +0200

    This is the first commit

---
+++ src/store/actionCreators.ts
@@ -0,0 +1,23 @@
+import * as actionTypes from "./actionTypes"
+
+export function  addArticle(article: IArticle){
+    const  action: ArticleAction = {
+        type: actionTypes.ADD_ARTICLE, article
+    }
+
+    return simulateHttpRequest(action)
+}
+
+
+export function  removeArticle(article:IArticle){
+    const  action: ArticleAction = {
+        type: actionTypes.REMOVE_ARTICLE, article
+    }
+}
+function  simulateHttpRequest(action: ArticleAction) {
+    return (dispatch: DispatchType) => {
+        setTimeout(() =>{
+            dispatch((action))
+        })
+    }
+}
---
+++ src/store/actionTypes.ts
@@ -0,0 +1,4 @@
+
+
+export const ADD_ARTICLE = "ADD_ARTICLE"
+export const  REMOVE_ARTICLE = "REMOVE_ARTICLE"
